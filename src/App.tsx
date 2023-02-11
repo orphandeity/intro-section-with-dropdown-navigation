c255bf2d18645fd5ba34e0fd61abf142f54af447
orphandeity <88175879+orphandeity@users.noreply.github.com>
2023-02-11 12:54:26 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+function App() {
+  return (
+    <main>
+      <nav>
+        <div>Features</div>
+        <div>Company</div>
+        <div>Careers</div>
+        <div>About</div>
+      </nav>
+
+      <button>Login</button>
+      <button>Register</button>
+
+      <h1>Make remote work</h1>
+
+      <p>
+        Get your team in sync, no matter your location. Streamline processes,
+        create team rituals, and watch productivity soar.
+      </p>
+
+      <button>Learn more</button>
+    </main>
+  );
+}
+
+const Attribution = () => {
+  return (
+    <footer>
+      Challenge by{' '}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by{' '}
+      <a href="https://www.frontendmentor.io/profile/orphandeity">
+        Jeff R Williams
+      </a>
+      .
+    </footer>
+  );
+};
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        '_almost-white': '#fafafa',
+        '_medium-gray':  '#696969',
+        '_almost-black': '#141414'
+      },
+      fontFamily: {
+        'sans': ['Epilogue', 'sans-serif']
+      },
+      screens: {
+        'mobile': '375px',
+        'desktop': '1440px'
+      },
+    },
+  },
+  plugins: [],
+}
